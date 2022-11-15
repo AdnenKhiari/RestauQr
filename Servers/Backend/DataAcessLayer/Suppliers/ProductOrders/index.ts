12ffe8da9f8dafdc706af02dbada5180f0f40706
AdnenKhiari <adnenkhiari484@gmail.com>
2022-11-15 18:38:45 +0100

    Fixed error handling Bug: was not awaiting the promises containing the error thrown wich caused them to not be correctly catched in Product Order Update / Add

--- Servers/Backend/DataAcessLayer/Suppliers/ProductOrders/index.ts
+++ Servers/Backend/DataAcessLayer/Suppliers/ProductOrders/index.ts
@@ -74,82 +74,83 @@ const AddUpdateProductOrder = async (data: any,supplierid: string,id: string | u
     const db = admin.firestore()
     const path = 'suppliers/'+supplierid+"/productorders/"
     const current_order_ref = id ? db.doc(path+id) : db.collection(path).doc() 
+
     await db.runTransaction(async tr=>{
-        const ids : any[] = []
-        const to_update = data.orders.filter((order: any)=>{
-            return order.id !== undefined
-        })
-        if(id){
-            const current_order_snap = await tr.get(current_order_ref)
-            if(!current_order_snap.exists)
-                throw Error("Invalid Id")
-            const current_order : any = current_order_snap.data()
-            console.warn("This is lel",current_order,to_update,data.orders)
-
-            //to make sure you can update the status and the quantities 
-            if(to_update.length > 0){
-                const to_update_snaps = await tr.getAll(...to_update.map((order: any)=>db.doc('products/'+order.product+"/product_instances/"+order.productorder_details.id)))
-                to_update_snaps.forEach(async (dl,key: number)=>{
+        
+            const ids : any[] = []
+            const to_update = data.orders.filter((order: any)=>{
+                return order.id !== undefined
+            })
+            if(id){
+                const current_order_snap = await tr.get(current_order_ref)
+                if(!current_order_snap.exists)
+                    throw Error("Invalid Id")
+                const current_order : any = current_order_snap.data()
+               // console.warn("This is lel",current_order,to_update,data.orders)
     
-                    if(!dl.exists)
-                        throw Error("Merchandise Not Found")
-                    //merchandise instance
-                    const current_mechandise : any = dl.data()
-                    const futur_productorder = to_update[key]
-                    const current_productorder = current_order.orders.find((ord: any)=> ord.id === futur_productorder.id )
-                    console.warn("This is lel",current_productorder,futur_productorder)
-
-                    if(!(current_productorder.details.status === "Completed" && futur_productorder.details.status === "Completed")){
-                        if((current_productorder.details.status === "Completed" && futur_productorder.details.status !== "Completed")){
-                            throw Error("Invalid Status Changes")
+                //to make sure you can update the status and the quantities 
+                if(to_update.length > 0){
+                    const to_update_snaps = await tr.getAll(...to_update.map((order: any)=>db.doc('products/'+order.product+"/product_instances/"+order.productorder_details.id)))
+                    await Promise.all(to_update_snaps.map(async (dl,key: number)=>{
+                        if(!dl.exists)
+                            throw Error("Merchandise Not Found")
+                        //merchandise instance
+                        const current_mechandise : any = dl.data()
+                        const futur_productorder = to_update[key]
+                        const current_productorder = current_order.orders.find((ord: any)=> ord.id === futur_productorder.id )
+                        console.warn("This is lel",current_productorder,futur_productorder)
+    
+                        if(!(current_productorder.details.status === "Completed" && futur_productorder.details.status === "Completed")){
+                            if((current_productorder.details.status === "Completed" && futur_productorder.details.status !== "Completed")){
+                                throw Error("Invalid Status Changes")
+                            }
+                            if((current_productorder.details.status === "Waiting" && futur_productorder.details.status === "Completed")){
+                                const new_quantity = current_mechandise.unitQuantity * current_mechandise.productQuantity
+                                const prodref = db.doc('products/'+current_productorder.product)
+                                //add merchandise quantity to stock
+                                tr.update(prodref,{
+                                    stockQuantity: admin.firestore.FieldValue.increment(new_quantity)
+                                })           
+                                //make merchandise enabled  
+                                const ref = db.doc('products/'+current_productorder.product+'/product_instances/'+current_productorder.merchandiseid)
+                                tr.update(ref,{
+                                    disabled: false
+                                })       
+                            }else if(current_productorder.details.status === "Canceled" && futur_productorder.details.status !== "Canceled" ){
+                                throw Error("Invalid Status Change")
+                            }
                         }
-                        if((current_productorder.details.status === "Waiting" && futur_productorder.details.status === "Completed")){
-                            const new_quantity = current_mechandise.unitQuantity * current_mechandise.productQuantity
-                            const prodref = db.doc('products/'+current_productorder.product)
-                            //add merchandise quantity to stock
-                            tr.update(prodref,{
-                                stockQuantity: admin.firestore.FieldValue.increment(new_quantity)
-                            })           
-                            //make merchandise enabled  
-                            const ref = db.doc('products/'+current_productorder.product+'/product_instances/'+current_productorder.merchandiseid)
-                            tr.update(ref,{
-                                disabled: false
-                            })       
-                        }else if(current_productorder.details.status === "Canceled" && futur_productorder.details.status !== "Canceled" ){
-                            throw Error("Invalid Status Change")
-                        }
+                    })) 
+                }
+            }
+    
+            data.orders.forEach((order: any,key: number)=>{
+                const productorderid = order.id
+                const prodref = db.doc('products/'+order.product)
+                if(!productorderid){
+                    const ref = db.collection('products/'+order.product+'/product_instances/').doc()
+                    addmerchandise(tr,prodref,ref,order.productorder_details,order.details.status !== "Completed")
+                    ids.push({merchandiseid: ref.id,id: uuidv4() })
+                }else{
+                    ids.push({merchandiseid: order.productorder_details.id ,id: productorderid })
+                }
+            })   
+    
+            tr.set(current_order_ref,{
+                notes: data.notes,
+                expected_delivery_date: data.expected_delivery_date,
+                orders: ids.map((d,key: number)=>{
+                    return {
+                        id: d.id,
+                        merchandiseid:d.merchandiseid,
+                        details: data.orders[key].details,
+                        product: data.orders[key].product 
                     }
                 })
-            }
-    
-        }
-
-        data.orders.forEach((order: any,key: number)=>{
-            const productorderid = order.id
-            const prodref = db.doc('products/'+order.product)
-            if(!productorderid){
-                const ref = db.collection('products/'+order.product+'/product_instances/').doc()
-                addmerchandise(tr,prodref,ref,order.productorder_details,order.details.status !== "Completed")
-                ids.push({merchandiseid: ref.id,id: uuidv4() })
-            }else{
-                ids.push({merchandiseid: order.productorder_details.id ,id: productorderid })
-            }
-        })   
-
-        tr.set(current_order_ref,{
-            notes: data.notes,
-            expected_delivery_date: data.expected_delivery_date,
-            orders: ids.map((d,key: number)=>{
-                return {
-                    id: d.id,
-                    merchandiseid:d.merchandiseid,
-                    details: data.orders[key].details,
-                    product: data.orders[key].product 
-                }
-            })
-        }) 
-
+            }) 
+       
     })
+
     return current_order_ref.id
 
 }
